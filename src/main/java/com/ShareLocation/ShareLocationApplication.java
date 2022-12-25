f10563c996a403bf3ef59d72d7cbd9c72aac91f4
minsung-kim-cmd <waxds97@naver.com>
2022-12-25 10:11:20 +0900

    221225

--- src/main/java/com/ShareLocation/ShareLocationApplication.java
+++ src/main/java/com/ShareLocation/ShareLocationApplication.java
@@ -2,12 +2,18 @@ package com.ShareLocation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShareLocationApplication {
+public class ShareLocationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShareLocationApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ShareLocationApplication.class);
+	}
 }
